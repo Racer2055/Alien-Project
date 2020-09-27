14e5dde277edf5489572432805baf5d76698a1af
Anthony  Allam <anthony@Anthonys-MacBook-Pro-2.local>
2020-09-27 00:19:38 -0500

    Responding to right keypress

--- alien_invasion.py
+++ alien_invasion.py
@@ -39,9 +39,10 @@ class AlienInvasion:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                elif event.type == pygame.K_RIGHT:
-                    # Move the ship to the right
-                    self.ship.rect.x +=1
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        # Move the ship to the right
+                        self.ship.rect.x +=1
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
