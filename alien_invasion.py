fad69ca02e540f689e250d673ef34d6ebbcc54ee
Anthony  Allam <anthony@Anthonys-MacBook-Pro-2.local>
2020-09-27 03:27:12 -0500

    Only keeps certain parts of the game active

--- alien_invasion.py
+++ alien_invasion.py
@@ -40,9 +40,12 @@ class AlienInvasion:
 
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+                
             self._update_screen()
 
     def _create_fleet(self):
