29a925e4a98d8107b6d900bb8f971634285ae459
Anthony  Allam <anthony@Anthonys-MacBook-Pro-2.local>
2020-10-10 16:31:59 -0500

    Updates the score as aliens are hit

--- alien_invasion.py
+++ alien_invasion.py
@@ -132,7 +132,7 @@ class AlienInvasion:
 
         # Draw the score information
         self.sb.show_score()
-        
+
         # Draw the play button if the game is inactive
         if not self.stats.game_active:
             self.play_button.draw_button()
@@ -175,6 +175,11 @@ class AlienInvasion:
         """ Respond to bullet-alien collisions."""
         # Remove any bullet and aliens that have collided.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if collisions:
+            self.stats.score += self.settings.alien_points
+            self.sb.prep_score()
+            
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
--- settings.py
+++ settings.py
@@ -29,6 +29,9 @@ class Settings:
         self.ship_speed = 1.5
         self.bullet_speed = 3.0
         self.alien_speed = 1.0
+        
+        # Scoring
+        self.alien_points = 50
 
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
