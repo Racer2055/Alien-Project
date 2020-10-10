6a85fb3daca2d78d5670e5cce17aedde7dcdbad1
Anthony  Allam <anthony@Anthonys-MacBook-Pro-2.local>
2020-10-10 14:11:45 -0500

    Created the method _check_play_button

--- alien_invasion.py
+++ alien_invasion.py
@@ -185,6 +185,11 @@ class AlienInvasion:
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     mouse_pos = pygame.mouse.get_pos()
                     self._check_play_button(mouse_pos)
+                    
+    def _check_play_button(self, mouse_pos):
+        """ Start a new game when the player clicks Play."""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
 
     def _check_keydown_events(self,event):
         """Responds to keypresses."""
