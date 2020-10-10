b88fe433777a6bbfe79bdee2e715e2dcc91a90b5
Anthony  Allam <anthony@Anthonys-MacBook-Pro-2.local>
2020-10-10 16:52:27 -0500

    Displays high score

--- game_stats.py
+++ game_stats.py
@@ -6,6 +6,9 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
+        # High score should never be reset
+        self.high_score = 0
+
         # Start Alien Invasion in an active state.
         self.game_active = False
     
@@ -13,4 +16,3 @@ class GameStats:
         """ Initialize statistcs that can change during the game."""
         self.ships_left = self.settings.ship_limit 
         self.score = 0
-        
